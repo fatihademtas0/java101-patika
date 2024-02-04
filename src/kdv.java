47ae0a6a2723f72c2990985acfa4849dd585b7b3
fatihademtas0 <fatihademtas@outlook.com>
2024-02-04 20:54:21 +0300

    first commit

---
+++ src/kdv.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class kdv {
+    public static void main(String[] args) {
+        System.out.println("Enter the radius of the circle.");
+        Scanner input = new Scanner(System.in);
+
+        double radius = input.nextDouble();
+        double pi = 3.14;
+
+        double circumference = 2*pi*radius;
+
+        double area = pi*radius*radius;
+
+        System.out.println("Circles circumference = "+circumference);
+        System.out.println("Circles area = "+area);
+
+    }
+}

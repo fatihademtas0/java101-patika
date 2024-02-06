fd79d67b1fd6bb1cf3dae6b77551123074deaddc
fatihademtas0 <fatihademtas@outlook.com>
2024-02-06 13:48:05 +0300

    advice

--- src/login.java
+++ src/login.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 
 public class login {
     public static void main(String[] args) {
-        String userName , password;
+        String userName, password;
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Please enter yout user name : ");
+        System.out.print("Please enter your user name : ");
         userName = input.nextLine();
 
         System.out.print("Please enter your password : ");
         password = input.nextLine();
 
-        if( userName.equals("patika") && password.equals("java123")){
+        if (userName.equals("patika") && password.equals("java123")) {
             System.out.println("You logged in successfully!");
-        }else{
-            System.out.println("You entered wrong user name or password.");
-            System.out.println("Please try again.");
+        } else {
+            System.out.println("You entered wrong password.");
+
         }
     }
 }

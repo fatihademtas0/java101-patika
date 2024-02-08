8032a5324553719f158f65fd433fa208966ed3df
fatihademtas0 <fatihademtas@outlook.com>
2024-02-08 19:02:14 +0300

    even number

---
+++ src/isEven.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class isEven {
+    public static void main(String[] args) {
+        System.out.print("Enter a number : ");
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+
+        for( int i = 0; i<=num;i++){
+            if( i %2 == 0){
+                System.out.println(i+" *");
+            }
+        }
+    }
+}

1c6a9d1c5e1b112e4d4692544a1dce6273f7abb3
fatihademtas0 <fatihademtas@outlook.com>
2024-02-08 16:59:42 +0300

    leap year

---
+++ src/leapyear.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class leapyear {
+    public static void main(String[] args) {
+        System.out.print("Please enter the year : ");
+        Scanner input = new Scanner(System.in);
+
+        double year = input.nextDouble();
+
+        if( year %100 == 0){
+            if ( year %4 == 0 && year %400 == 0 ){
+                System.out.println("İt is a leap year!");
+            }else {
+                System.out.println("İt is not a leap year!");
+            }
+        }else{
+            if (year %4 == 0){
+                System.out.println("İt is a leap year!");
+            }else {
+                System.out.println("İt is not a leap year!");
+            }
+        }
+    }
+}

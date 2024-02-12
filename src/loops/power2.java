8b90268dc9726e59de12bb12e0f0770f9e948f45
fatihademtas0 <fatihademtas@outlook.com>
2024-02-12 14:31:28 +0300

    power

--- src/loops/power2.java
+++ src/loops/power2.java
@@ -6,15 +6,15 @@ public class power2 {
     public static void main(String[] args) {
         System.out.print("Enter a number : ");
         Scanner input = new Scanner(System.in);
-        int num = input.nextInt();
+        int number = input.nextInt();
 
         System.out.print("Enter the power : ");
         int pow = input.nextInt();
 
         int total = 1;
         for (int i = 1; i <= pow; i++) {
-            total *= num;
+            total *= number;
         }
-        System.out.println(num + " ^ " + pow + " = " + total);
+        System.out.println(number + " ^ " + pow + " = " + total);
     }
 }

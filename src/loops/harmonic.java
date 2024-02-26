c47756eb105c5691725796aafeca66bbe0242774
fatihademtas0 <fatihademtas@outlook.com>
2024-02-27 00:38:38 +0300

    harmonic

---
+++ src/loops/harmonic.java
@@ -0,0 +1,18 @@
+package loops;
+
+import java.util.Scanner;
+
+public class harmonic {
+    public static void main(String[] args) {
+        System.out.print("Enter a number : ");
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+
+        double result = 0;
+
+        for (double i = 1; i<=num;i++){
+            result += 1/i;
+        }
+        System.out.println(result);
+    }
+}

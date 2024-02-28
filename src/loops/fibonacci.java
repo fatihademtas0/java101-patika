e8360c641f814cd45e6fc0e088710a018a1f6ad6
fatihademtas0 <fatihademtas@outlook.com>
2024-02-29 02:36:54 +0300

    prime numbers

---
+++ src/loops/fibonacci.java
@@ -0,0 +1,21 @@
+package loops;
+
+import  java.util.Scanner;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements of the Fibonacci series : ");
+        int nums = input.nextInt();
+
+        int firstNum = 0, secondNum = 1, sum;
+
+        for (int i = 0; i <= nums; i++) {
+            System.out.println(firstNum+ " + "+secondNum+" = "+(firstNum+secondNum));
+            sum = firstNum + secondNum;
+            firstNum = secondNum;
+            secondNum = sum;
+        }
+    }
+}

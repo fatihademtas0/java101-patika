e56fc89fc0878874eee2245a6b13a28f95e231bc
fatihademtas0 <fatihademtas@outlook.com>
2024-03-19 01:46:07 +0300

    Transpose Matrix

---
+++ src/Arrays/Transpose.java
@@ -0,0 +1,43 @@
+package Arrays;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Transpose {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the row number : ");
+        int row = input.nextInt();
+
+        System.out.print("Enter the column number : ");
+        int col = input.nextInt();
+
+        int[][] list = new int[row][col];
+        int[][] transposeList = new int[col][row];
+
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list[i].length; j++) {
+                System.out.print("Enter the " + i + ". row, " + j + ". column of the matrix : ");
+                int element = input.nextInt();
+                list[i][j] = element;
+            }
+        }
+        System.out.println("Matrix : ");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(list[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("Transpose Matrix : ");
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < row; j++) {
+                System.out.print(list[j][i] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}

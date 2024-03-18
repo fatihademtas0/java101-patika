35e373dda6b7458c64cca8c1874267490d89fd88
fatihademtas0 <fatihademtas@outlook.com>
2024-03-19 02:45:13 +0300

    Math Class

---
+++ src/Standart_Classes/Mathh.java
@@ -0,0 +1,27 @@
+package Standart_Classes;
+
+public class Mathh {
+    public static void main(String[] args) {
+        System.out.println(Math.max(3000,200)); // prints the bigger number
+
+        System.out.println(Math.min(31,590)); // prints the smallest number
+
+        System.out.println(Math.exp(2)); // prints e^2
+
+        System.out.println(Math.pow(2,3)); // prints the third power of 2
+
+        System.out.println(Math.random()); // prints a number between 0-1
+
+        System.out.println(Math.sqrt(25)); // prints square root of the given number
+
+        System.out.println(Math.round(6.4)); // rounds to the closest ınteger
+
+        System.out.println(Math.ceil(6.0000000000001)); // rounds to the next ınteger
+
+        System.out.println(Math.floor(3.9)); // rounds to the previous ınteger
+
+        long value = -3344433332232222233L;
+        System.out.println(Math.abs(value)); //returns the absolute value of the number
+
+    }
+}
